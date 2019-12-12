0ad5074f86f96454660307e80e4062acea45f05b
vhgiron-u <vhgiron@pucp.edu.pe>
2019-12-12 18:07:16 -0500

    Se agrega paralelismo

--- multicam.py
+++ multicam.py
@@ -1,5 +1,9 @@
 import numpy as np
 import cv2
+from collections import Counter
+from time import time
+
+DISPLAY_CAMS = True
 
 
 frame_width = 640
@@ -7,18 +11,18 @@ frame_height = 480
 
 
 #para mostrar en pantalla lo que se esta grabando
-screen_width = 1920
-#screen_width = frame_width
-screen_height = 1080
-#screen_height = frame_height
+#screen_width = 1920
+screen_width = frame_width
+#screen_height = 1080
+screen_height = frame_height
 
 
 fps_cam = 120
-fps_vid = 60
+fps_vid = 120
 
-#cap_1 = cv2.VideoCapture(0)
+cap_1 = cv2.VideoCapture(0)
 #cap_2 = cv2.VideoCapture(1)
-cap_1 = cv2.VideoCapture(1)
+#cap_1 = cv2.VideoCapture(1)
 cap_2 = cv2.VideoCapture(2)
 cap_3 = cv2.VideoCapture(3)
 
@@ -41,7 +45,9 @@ ret1, frame1 = cap_1.read()
 ret2, frame2 = cap_2.read()
 ret3, frame3 = cap_3.read()
 
-
+print("cap_1:",cap_1.get(cv2.CAP_PROP_FPS))
+print("cap_2:",cap_2.get(cv2.CAP_PROP_FPS))
+print("cap_3:",cap_3.get(cv2.CAP_PROP_FPS))
 
 out1 = cv2.VideoWriter(video_output_1, cv2.VideoWriter_fourcc('M','J','P','G'),fps_vid,(frame_width,frame_height),True)
 
@@ -51,31 +57,44 @@ out3 = cv2.VideoWriter(video_output_3, cv2.VideoWriter_fourcc('M','J','P','G'),f
 
 cont = 0
 
+#depuracion de tasa de fallos
+rets = dict()
+rets[1] = list()
+rets[2] = list()
+rets[3] = list()
+
+t0 = time()
 while True:
     # Capture frame-by-frame
     ret1, frame1 = cap_1.read()
     ret2, frame2 = cap_2.read()
     ret3, frame3 = cap_3.read()
+    rets[1].append(ret1)
+    rets[2].append(ret2)
+    rets[3].append(ret3)
 
     if ret1==True:
         # Display the resulting frame
         cv2.namedWindow('Camera1',cv2.WINDOW_NORMAL)
         cv2.resizeWindow('Camera1', (screen_width,screen_height))
-        cv2.imshow('Camera1', frame1)
+        if DISPLAY_CAMS:
+            cv2.imshow('Camera1', frame1)
         out1.write(frame1)
 
     if ret2==True:
         # Display the resulting frame
         cv2.namedWindow('Camera2',cv2.WINDOW_NORMAL)
         cv2.resizeWindow('Camera2', (screen_width,screen_height))
-        cv2.imshow('Camera2', frame2)
+        if DISPLAY_CAMS:
+            cv2.imshow('Camera2', frame2)
         out2.write(frame2)
 
     if ret3==True:
         # Display the resulting frame
         cv2.namedWindow('Camera3',cv2.WINDOW_NORMAL)
         cv2.resizeWindow('Camera3', (screen_width,screen_height))
-        cv2.imshow('Camera3', frame3)
+        if DISPLAY_CAMS:
+            cv2.imshow('Camera3', frame3)
         out3.write(frame3)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -88,11 +107,25 @@ while True:
         #cv2.imwrite('F1.jpg',frame1)
         #cv2.imwrite('F2.jpg',frame2)
         cont = cont + 1
+t1 = time()
+
+print("al terminar el programa: cap_1.CAP_PROP_FPS:",cap_1.get(cv2.CAP_PROP_FPS))
+print("al terminar el programa: cap_2.CAP_PROP_FPS:",cap_2.get(cv2.CAP_PROP_FPS))
+print("al terminar el programa: cap_3.CAP_PROP_FPS:",cap_3.get(cv2.CAP_PROP_FPS))
+t_grabacion = t1-t0
+frames_totales = len(rets[3])
+print("frames capturados:", frames_totales)
+print("tiempo de grabacion:", t_grabacion)
+print("fps empirico:", frames_totales/t_grabacion)
+
+print("Cantidades finales de frames capturados:")
+print({k:Counter(R) for k,R in rets.items()})
 
 
 # When everything is done, release the capture
 cap_1.release()
 cap_2.release()
+cap_3.release()
 
 #close all the frames
 cv2.destroyAllWindows()
