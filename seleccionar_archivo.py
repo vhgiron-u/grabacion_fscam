b0a6f241ce83e71601eb151fc18c69a725184873
vhgiron-u <vhgiron@pucp.edu.pe>
2020-01-05 22:09:42 -0500

    Se puede seleccionar carpeta graficamente

--- seleccionar_archivo.py
+++ seleccionar_archivo.py
@@ -1,7 +1,12 @@
 from tkinter import Tk
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename, askdirectory
 
 def seleccionar_archivo(titulo):
 	Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 	filename = askopenfilename(title=titulo) # show an "Open" dialog box and return the path to the selected file
 	return filename
+
+def seleccionar_carpeta(titulo):
+    Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    filename = askdirectory(title=titulo) # show an "Open" dialog box and return the path to the selected file
+    return filename
