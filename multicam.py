901bc64bb05d66d64acb353e7e35cee4cc65427d
vhgiron-u <vhgiron@pucp.edu.pe>
2020-02-06 18:02:39 -0500

    Se vuelve a crear multicam.py a partir de singlecam

--- multicam.py
+++ multicam.py
@@ -2,130 +2,144 @@ import numpy as np
 import cv2
 from collections import Counter
 from time import time
+import logging
+import sys
+from seleccionar_archivo import seleccionar_carpeta
+
+#Constantes y valores a setear
+
+CONFIGS = {
+    "VGA":{
+        "frame_width": 640,
+        "frame_height": 480,
+        "fps_cam": 120,
+    },
+    "1080P":{
+        "frame_width": 1920,
+        "frame_height": 1080,
+        "fps_cam": 60,
+    }
+}
 
 DISPLAY_CAMS = True
 
-
-frame_width = 640
-frame_height = 480
+CAM_INDEXES = [1,2]
 
 
-#para mostrar en pantalla lo que se esta grabando
-#screen_width = 1920
-screen_width = frame_width
-#screen_height = 1080
-screen_height = frame_height
+def main(ruta=None, conf_name = "VGA"):
+    if conf_name not in CONFIGS:
+        raise ValueError("Nombre de configuracion '{}' no reconocido".format(conf_name))
+
+    if ruta is None:
+        ruta_carpeta = seleccionar_carpeta("Seleccionar carpeta")
+    else:
+        ruta_carpeta = ruta
+    nombre_carpeta = ruta_carpeta.split('/')[-1]
+
+    current_config_name = conf_name
+    current_config_options = CONFIGS[current_config_name]
+
+    frame_width = current_config_options["frame_width"]
+    frame_height = current_config_options["frame_height"]
+
+    #para mostrar en pantalla lo que se esta grabando
+    screen_width = frame_width
+    screen_height = frame_height
+
+    fps_cam = current_config_options['fps_cam']
+    fps_vid = fps_cam
 
 
-fps_cam = 120
-fps_vid = 120
+    caps = dict()
+    video_outputs = dict()
+    for ix in CAM_INDEXES:
+        vcap = cv2.VideoCapture(CAM_INDEXES[0], cv2.CAP_V4L2) #:specific_cam:
+        vcap.set(cv2.CAP_PROP_FPS, fps_cam) #:specific_cam:
+        vcap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_width) #:specific_cam:
+        vcap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_height) #:specific_cam:
+        caps[ix] = vcap
+        video_outputs[ix] = '{}/{}_cam{}.avi'.format(ruta_carpeta, nombre_carpeta,ix) #:specific_cam:
+    
 
-cap_1 = cv2.VideoCapture(0)
-#cap_2 = cv2.VideoCapture(1)
-#cap_1 = cv2.VideoCapture(1)
-cap_2 = cv2.VideoCapture(2)
-cap_3 = cv2.VideoCapture(3)
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    fh = logging.FileHandler(ruta_carpeta+'/' +nombre_carpeta+'_'+r'log.txt')
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+    logger.addHandler(fh)
 
-cap_1.set(cv2.CAP_PROP_FPS, fps_cam)
-cap_2.set(cv2.CAP_PROP_FPS, fps_cam)
-cap_3.set(cv2.CAP_PROP_FPS, fps_cam)
+    logger.info("ruta de la carpeta que guardara el archivo:")
+    logger.info(ruta_carpeta)
+    for ix in CAM_INDEXES:
+        logger.info("cap_{} al inicio del programa: ".format(ix) + str(caps[ix].get(cv2.CAP_PROP_FPS))) #:specific_cam:
 
-cap_1.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_width)
-cap_1.set(cv2.CAP_PROP_FRAME_WIDTH, frame_height)
-cap_2.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_width)
-cap_2.set(cv2.CAP_PROP_FRAME_WIDTH, frame_height)
-cap_3.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_width)
-cap_3.set(cv2.CAP_PROP_FRAME_WIDTH, frame_height)
+    outs = dict()
+    for ix in CAM_INDEXES:
+        outs[ix] = cv2.VideoWriter(video_outputs[ix], cv2.VideoWriter_fourcc('M','J','P','G'),fps_vid,(frame_width,frame_height),True)  #:specific_cam:
 
-video_output_1 = 'cam1.avi'
-video_output_2 = 'cam2.avi'
-video_output_3 = 'cam3.avi'
+    cont = 0
+    #depuracion de tasa de fallos por camara
+    rets = dict()
+    frames = [None]*len(CAM_INDEXES) #frames es una lista indexada en 0
+    cam_names = {ix:"Camera{}".format(ix) for ix in CAM_INDEXES}
+    for ix in CAM_INDEXES:
+        rets[ix] = list() #:specific_cam:
 
-ret1, frame1 = cap_1.read()
-ret2, frame2 = cap_2.read()
-ret3, frame3 = cap_3.read()
+    t0 = time()
+    while True:
+        # Capture frame-by-frame
+        for i_, ix in enumerate(CAM_INDEXES):
+            ret, frame = caps[ix].read() #:specific_cam:
+            rets[ix].append(ret) #:specific_cam:
+            frames[i_] = frame
 
-print("cap_1:",cap_1.get(cv2.CAP_PROP_FPS))
-print("cap_2:",cap_2.get(cv2.CAP_PROP_FPS))
-print("cap_3:",cap_3.get(cv2.CAP_PROP_FPS))
+        for i_, ix in enumerate(CAM_INDEXES):
+            ret, frame = rets[ix][-1], frames[i_]
+            cam_name = cam_names[ix]
+            if ret==True: #:specific_cam:
+                # Display the resulting frame
 
-out1 = cv2.VideoWriter(video_output_1, cv2.VideoWriter_fourcc('M','J','P','G'),fps_vid,(frame_width,frame_height),True)
-
-out2 = cv2.VideoWriter(video_output_2, cv2.VideoWriter_fourcc('M','J','P','G'),fps_vid,(frame_width,frame_height),True)
-
-out3 = cv2.VideoWriter(video_output_3, cv2.VideoWriter_fourcc('M','J','P','G'),fps_vid,(frame_width,frame_height),True)
-
-cont = 0
-
-#depuracion de tasa de fallos
-rets = dict()
-rets[1] = list()
-rets[2] = list()
-rets[3] = list()
-
-t0 = time()
-while True:
-    # Capture frame-by-frame
-    ret1, frame1 = cap_1.read()
-    ret2, frame2 = cap_2.read()
-    ret3, frame3 = cap_3.read()
-    rets[1].append(ret1)
-    rets[2].append(ret2)
-    rets[3].append(ret3)
-
-    if ret1==True:
-        # Display the resulting frame
-        cv2.namedWindow('Camera1',cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('Camera1', (screen_width,screen_height))
-        if DISPLAY_CAMS:
-            cv2.imshow('Camera1', frame1)
-        out1.write(frame1)
-
-    if ret2==True:
-        # Display the resulting frame
-        cv2.namedWindow('Camera2',cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('Camera2', (screen_width,screen_height))
-        if DISPLAY_CAMS:
-            cv2.imshow('Camera2', frame2)
-        out2.write(frame2)
-
-    if ret3==True:
-        # Display the resulting frame
-        cv2.namedWindow('Camera3',cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('Camera3', (screen_width,screen_height))
-        if DISPLAY_CAMS:
-            cv2.imshow('Camera3', frame3)
-        out3.write(frame3)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-    if cv2.waitKey(1) & 0xFF == ord('a'):
-        print('foto!')
-        cv2.imwrite('F1-'+str(cont)+'.jpg',frame1)
-        cv2.imwrite('F2-'+str(cont)+'.jpg',frame2)
-        #cv2.imwrite('F1.jpg',frame1)
-        #cv2.imwrite('F2.jpg',frame2)
-        cont = cont + 1
-t1 = time()
-
-print("al terminar el programa: cap_1.CAP_PROP_FPS:",cap_1.get(cv2.CAP_PROP_FPS))
-print("al terminar el programa: cap_2.CAP_PROP_FPS:",cap_2.get(cv2.CAP_PROP_FPS))
-print("al terminar el programa: cap_3.CAP_PROP_FPS:",cap_3.get(cv2.CAP_PROP_FPS))
-t_grabacion = t1-t0
-frames_totales = len(rets[3])
-print("frames capturados:", frames_totales)
-print("tiempo de grabacion:", t_grabacion)
-print("fps empirico:", frames_totales/t_grabacion)
-
-print("Cantidades finales de frames capturados:")
-print({k:Counter(R) for k,R in rets.items()})
+                cv2.namedWindow(cam_name,cv2.WINDOW_NORMAL) #:specific_cam:
+                cv2.resizeWindow(cam_name, (screen_width,screen_height)) #:specific_cam:
+                if DISPLAY_CAMS: #:specific_cam:
+                    cv2.imshow(cam_name, frame) #:specific_cam:
+                outs[ix].write(frame) #:specific_cam:
 
 
-# When everything is done, release the capture
-cap_1.release()
-cap_2.release()
-cap_3.release()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-#close all the frames
-cv2.destroyAllWindows()
+        if cv2.waitKey(1) & 0xFF == ord('a'):
+            logger.info('foto!')
+            for i_, ix in enumerate(CAM_INDEXES):
+                cv2.imwrite('F{}-'.format(ix)+str(cont)+'.jpg',frames[i_]) #:specific_cam:
+                cont = cont + 1
+    t1 = time()
+    for ix in CAM_INDEXES:
+        
+        cap = caps[ix]
+        logger.info("al terminar el programa: caps[{}].CAP_PROP_FPS: {}".format(cap,cap.get(cv2.CAP_PROP_FPS)))
+
+    t_grabacion = t1-t0
+
+    frames_totales = len(rets[CAM_INDEXES[0]]) #:specific_cam:
+
+    logger.info("frames capturados: " + str(frames_totales)) #:specific_cam:
+    logger.info("tiempo de grabacion (segundos): " + str(t_grabacion))
+    logger.info("fps empirico:" + str(frames_totales/t_grabacion)) #:specific_cam:
+    logger.info("Cantidades finales de frames capturados por camara:")
+    logger.info({k:Counter(R) for k,R in rets.items()})
+
+
+    # When everything is done, release the capture
+    for ix in CAM_INDEXES:
+        caps[ix].release() #:specific_cam:
+
+    #close all the frames
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        main()
